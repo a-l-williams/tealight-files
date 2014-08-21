a212721ea97b7a889553f6c406cb484b8a78d10c
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:58:29 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -3,8 +3,8 @@ from github.lordvile018.art.racetrack import handle_keydown, handle_keyup
 
 currentDirection = 0
 
-
-draw_triangle(50,50,currentDirection,20,"red")
+def handle_frame():
+  draw_triangle(50,50,currentDirection,20,"red")
 
 
 #print test_polygon(100, 100, [(100,200), (50, 50), (200,100), (150,250)])
