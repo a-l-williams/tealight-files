2b09d823a74f1d667dba78dc90fa270ccef1208c
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:57:41 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,9 +1,10 @@
 from github.calintat.art.racetrack import draw_triangle
 from github.lordvile018.art.racetrack import handle_keydown, handle_keyup
-#from github.a-l-williams.art.project-globals
-#print Globals().get_connection_string()
 
-draw_triangle(50,50,60,20,"red")
+currentDirection = 0
+
+
+draw_triangle(50,50,currentDirection,20,"red")
 
 
 #print test_polygon(100, 100, [(100,200), (50, 50), (200,100), (150,250)])
