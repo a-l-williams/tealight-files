c474cb7416c8147d932352059bc5779e5aa3e452
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 11:23:01 +0100

    Running art/input.py

--- art/input.py
+++ art/input.py
@@ -1,10 +1,2 @@
-from tealight.art import (color, line, spot, circle, box, image, text, background)
-
-def handle_mousedown(x,y):
-  color("blue")
-  spot(x,y,10)
-  
-def handle_mousemove(x,y,button):
-  if button == "left":
-    color("red")
-    circle(x,y,10)
+import uuid
+print uuid.hex()
