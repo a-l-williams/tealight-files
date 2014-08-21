453bfe69483c2724baaef84f2b21648688cf41ed
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:51:50 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,5 +1,4 @@
 from github.calintat.art.racetrack import draw_triangle
-#from tealight.art import *
 #from github.lordvile018.art.racetrack import handle_keydown, handle_keyup
 #from github.a-l-williams.art.project-globals
 #print Globals().get_connection_string()
