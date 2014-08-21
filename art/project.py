0e1c66e9a1acc26c5fb1223edb1b9e8a9885cb81
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:48:05 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -4,7 +4,7 @@ from tealight.art import *
 #from github.a-l-williams.art.project-globals
 #print Globals().get_connection_string()
 
-draw_triangle(50,50,60,20,"blue")
+draw_triangle(0,0,60,20,"blue")
 
 
 #print test_polygon(100, 100, [(100,200), (50, 50), (200,100), (150,250)])
