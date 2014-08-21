28f9514f28a2dda457a661f5f4e4ef426a8e42ab
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:35:07 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,2 +1,5 @@
 from github.calintat.art.racetrack import draw_triangle
+from github.a-l-williams.art.project-globals
+print Globals().get_connection_string()
+
 draw_triangle(200,200,60,200,"blue")
