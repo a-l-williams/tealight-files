2b82d10690b5183ff3aeeb339f3c715e80716ace
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 14:45:57 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -52,7 +52,7 @@ def handle_frame():
   current_x = movement_data['x']
   current_y = movement_data['y']
   draw_triangle(current_x,current_y,current_direction,10,"red")
-  print current_velocity
+  #print current_velocity
   previous_direction = current_direction
 
 
