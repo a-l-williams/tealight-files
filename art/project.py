25def7ca4ae0b572a4537c939dd23d39c1c7a029
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 11:15:42 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,4 +1,4 @@
-#from github.calintat.art.racetrack import draw_triangle
+from github.calintat.art.racetrack import draw_triangle
 from github.lordvile018.art.racetrack import handle_keydown, handle_keyup
 from tealight.utils import github_load
 project_globals = github_load("a-l-williams", "art", "project-globals")
