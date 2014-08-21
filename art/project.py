9f3172b3b74b987ed926f15ea50e8b6fe64067be
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:42:14 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,9 +1,10 @@
-from github.calintat.art.racetrack import draw_triangle
-from tealight.art import *
+#from github.calintat.art.racetrack import draw_triangle
+#from tealight.art import *
+#from github.lordvile018.art.racetrack import handle_
 #from github.a-l-williams.art.project-globals
 #print Globals().get_connection_string()
 
 #draw_triangle(200,200,60,200,"blue")
 
 
-print test_polygon(110,110, [(100,100), (200,100), (150,250)])
+print polygon([(100,100), (200,100), (150,250)])
