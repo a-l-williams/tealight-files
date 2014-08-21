cb82534f67be248973cf3beeda4ac90ab72f6711
a-l-williams <adam.lu.williams@gmail.com>
2014-08-21 10:43:22 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -7,4 +7,4 @@ from tealight.art import *
 #draw_triangle(200,200,60,200,"blue")
 
 
-print test_polygon([(500, 500)], [(100,200), (50, 50), (200,100), (150,250)])
+print test_polygon(500, 500, [(100,200), (50, 50), (200,100), (150,250)])
