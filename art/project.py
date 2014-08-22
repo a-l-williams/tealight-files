dc2289ca0f648141b26cec09b13af508adbcc226
a-l-williams <adam.lu.williams@gmail.com>
2014-08-22 09:40:13 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -60,7 +60,7 @@ def handle_frame():
   text(5,5,"Score: " + str(score))
   movement_data = movement(current_x, current_y, current_velocity, current_direction, current_size)
   
-  if check_finish(current_x, current_y) == False:
+  if check_finish(current_x, current_y):
     color("white")
     print "winner"
     text(5,20,"You win!")
